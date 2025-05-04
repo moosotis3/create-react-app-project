f758f07b5c254d02de3365d6da22e229b7629330
moosotis3 <79678408+moosotis3@users.noreply.github.com>
2025-05-04 11:48:44 +0800

    Update date.go

--- api/date.go
+++ api/date.go
@@ -10,3 +10,5 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	currentTime := time.Now().Format(time.RFC850)
 	fmt.Fprintf(w, currentTime)
 }
+func Handler(w http.ResponseWriter, r *http.Request) {
+}
